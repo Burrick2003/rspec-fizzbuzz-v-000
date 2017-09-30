7441356f90f321a59b6861bf1b79925dfef94ab7
Blakeley Bond <burrick2003@yahoo.com>
2017-09-30 18:03:28 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ if input % 3 + input % 5 == 0
   puts "FizzBuzz"
 elseif input % 3 == 0 # if the number input is divisible by 3
     puts    "Fizz"
-
 elseif input % 5 == 0
     puts    "Buzz"
+else
 end
 
 
