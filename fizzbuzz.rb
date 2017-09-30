885ab2dd4a936fb4031192a89c7cb8b3d1944583
Blakeley Bond <burrick2003@yahoo.com>
2017-09-30 18:20:46 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,10 @@ def fizzbuzz(input)
   #input = gets.strip.to_i
   if input % 3 + input % 5 == 0
       "FizzBuzz"
+  end
   elseif input % 3 == 0 # if the number input is divisible by 3
       "Fizz"
+  end
   elseif input % 5 == 0
       "Buzz"
   end
