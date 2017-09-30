af2fa61ca9360f9d840f9ebac13ea5c32bdfb32f
Blakeley Bond <burrick2003@yahoo.com>
2017-09-30 17:56:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,18 @@
-#def fizzbuzz
+#def fizzbuzz(int)
   puts "Welcome to FizzBuzz!  Please enter an integer."
   input = gets.strip.to_i
-  puts(input)
+if input % 3 + input % 5 == 0
+  "FizzBuzz"
+  elseif input % 3 == 0 # if the number input is divisible by 3
+      "Fizz"
+    end
+  elseif input % 5 == 0
+      "Buzz"
+    end
+end
+  
+    
+      
       #if input
 
     #end
