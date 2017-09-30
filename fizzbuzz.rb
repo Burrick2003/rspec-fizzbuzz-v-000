5af96c9afb92e2b3f3fde0c965b840bc9b0d72fa
Blakeley Bond <burrick2003@yahoo.com>
2017-09-30 18:01:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
   input = gets.strip.to_i
 if input % 3 + input % 5 == 0
   puts "FizzBuzz"
-  elseif input % 3 == 0 # if the number input is divisible by 3
+end
+if input % 3 == 0 # if the number input is divisible by 3
     puts    "Fizz"
-
-  elseif input % 5 == 0
+end
+if input % 5 == 0
     puts    "Buzz"
-
 end
 
 
